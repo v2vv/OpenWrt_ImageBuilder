2948f916785014c6649b4556b987e1b3b5a9555d
haitianlan <31479870+v2vv@users.noreply.github.com>
2025-07-20 09:37:55 +0800

    Update post-files.sh

--- modules/ib/post-files.sh
+++ modules/ib/post-files.sh
@@ -1,16 +1,17 @@
-for config in $(printenv | grep '^CONFIG_'); do
-    config_name=$(echo $config | awk -F '=' '{print $1}')
-    config_value=$(echo $config | awk -F '=' '{print $2}')
-    sed -i "/$config_name/ c\\$config_name=$config_value" .config
+
+# 用 CONFIG_* 环境变量更新 .config
+printenv | grep '^CONFIG_' | while IFS='=' read -r config_name config_value; do
+    sed -i "s|^$config_name=.*|$config_name=$config_value|" .config
 done
 
-if [[ $PWD =~ "immortalwrt" ]]; then
+# 判断项目名称
+if echo "$PWD" | grep -q "immortalwrt"; then
     PROJECT_NAME="immortalwrt"
 else
     PROJECT_NAME="openwrt"
 fi
 
-
-if [ $USE_MIRROR = '1' ]; then
-    sed -i 's/https:\/\/downloads.'"$PROJECT_NAME"'.org/https:\/\/'"$MIRROR"'\/'"$PROJECT_NAME"'/g' ./repositories.conf
+# 如果启用镜像源
+if [ "$USE_MIRROR" = "1" ]; then
+    sed -i "s|https://downloads.$PROJECT_NAME.org|https://$MIRROR/$PROJECT_NAME|g" ./repositories.conf
 fi
